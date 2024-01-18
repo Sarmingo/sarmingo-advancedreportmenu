a0a6277b0d7c43039dd33fa78777e551be917e21
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2024-01-18 08:57:54 +0100

    Update main.lua
    update 1.1

--- server/main.lua
+++ server/main.lua
@@ -1,19 +1,19 @@
-ListaReportova    = {}
+ListOfReports    = {}
 local savedCoords = {}
 
 insert            = function(t, v)
     t[#t + 1] = v
 end
 
-RegisterNetEvent('napravitabelu', function(opis, naslov, kategorija)
+RegisterNetEvent('createtable', function(opis, naslov, kategorija, image)
     local xPlayer = ESX.GetPlayerFromId(source)
-
-    local noviReport = {
+    local newReport = {
         ime = GetPlayerName(source),
         opis = opis,
         naslov = naslov,
         kategorija = kategorija,
         id = source,
+        image = image,
         vrijeme = os.date("%H:%M:%S", os.time()),
         igime = xPlayer.getName(),
         posao = xPlayer.getJob().label .. ' - ' .. xPlayer.getJob().grade_label,
@@ -23,25 +23,26 @@ RegisterNetEvent('napravitabelu', function(opis, naslov, kategorija)
         identifier = xPlayer.identifier
     }
 
-    local jelpostoji = false
-    for _, report in pairs(ListaReportova) do
-        if report.id == noviReport.id and
-            report.opis == noviReport.opis and
-            report.kategorija == noviReport.kategorija and
-            report.naslov == noviReport.naslov then
-            jelpostoji = true
+    local alreadyexists = false
+    for _, report in pairs(ListOfReports) do
+        if report.id == newReport.id and
+            report.opis == newReport.opis and
+            report.kategorija == newReport.kategorija and
+            report.naslov == newReport.naslov then
+            alreadyexists = true
             break
         end
     end
 
-    if not jelpostoji then
-        insert(ListaReportova, noviReport)
+    if not alreadyexists then
+        insert(ListOfReports, newReport)
 
         local xPlayers = ESX.GetPlayers()
         for i = 1, #xPlayers, 1 do
             local admin = ESX.GetPlayerFromId(xPlayers[i])
-            if Config.DozvoljeneAdminGrupe[admin.getGroup()] then
-                TriggerClientEvent("sarmingo-report:salji-novi-report", admin.source, noviReport)
+            if Config.AllowedGroups[admin.getGroup()] then
+                TriggerClientEvent("sarmingo-report:salji-novi-report", admin.source, newReport)
+                logs('Send Report', 'Name: ' ..GetPlayerName(source).. '\nDescription: ' ..opis.. '\nTitle: ' ..naslov.. '\nCategory: ' ..kategorija.. '\nTime: ' ..os.date("%H:%M:%S", os.time()).. '\nIG Name: ' ..xPlayer.getName().. '\nJob: ' ..xPlayer.getJob().label .. ' - ' .. xPlayer.getJob().grade_label.. '\nBank: ' ..xPlayer.getAccount('bank').money.. '\nMoney: ' ..xPlayer.getMoney().. '\nGrupa: ' ..xPlayer.getGroup().. '\nIdentifier: ' ..xPlayer.identifier, image)
             end
         end
     end
@@ -49,41 +50,45 @@ end)
 
 RegisterNetEvent('posaljikeporu', function(id, poruka)
     TriggerClientEvent('posaljiporuku', id, poruka)
+    logs('Send Message', GetPlayerName(source).. " sent a message " ..poruka.. " to the player " ..GetPlayerName(target))
 end)
 
 RegisterNetEvent('dajitem', function(item, count, target)
     local xTarget = ESX.GetPlayerFromId(tonumber(target))
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         xTarget.addInventoryItem(item, tonumber(count) or 1)
+        logs('Give Item', GetPlayerName(source).. " gave the item " ..item.. " " ..count.. "x to the player " ..GetPlayerName(target))
     end
 end)
 
 RegisterNetEvent('dajvozilo', function(vehicle, target)
     local xTarget = ESX.GetPlayerFromId(tonumber(target))
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         TriggerClientEvent('dajvozilo:client', xTarget.source, vehicle)
+        logs('Give Vehicle', GetPlayerName(source).. " gave the vehicle to the player " ..GetPlayerName(target))
     end
 end)
 
 RegisterNetEvent('fixvehicle', function(target)
     local xTarget = ESX.GetPlayerFromId(tonumber(target))
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         TriggerClientEvent('fixvehicle:client', xTarget.source)
+        logs('Fix Vehicle', GetPlayerName(source).. " repaired the player's vehicle " ..GetPlayerName(target))
     end
 end)
 
 RegisterNetEvent('gotoplayer', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
     local xTarget = ESX.GetPlayerFromId(target)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         if xTarget then
             local targetCoords = xTarget.getCoords()
-            local playerCoords = xPlayer.getCoords()
             savedCoords[target] = targetCoords
-            xTarget.setCoords(playerCoords)
+            xPlayer.setCoords(targetCoords)
+            logs('Goto Player', GetPlayerName(source).. " went the player " ..GetPlayerName(target))
         end
     end
 end)
@@ -91,11 +96,12 @@ end)
 RegisterNetEvent('teleportback', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
     local xTarget = ESX.GetPlayerFromId(target)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         if xTarget then
             local playerCoords = savedCoords[target]
             if playerCoords then
                 xTarget.setCoords(playerCoords)
+                logs('Teleport Player Back', GetPlayerName(source).. ' has returned itself to its last location ')
                 savedCoords[target] = nil
             end
         end
@@ -106,12 +112,13 @@ end)
 RegisterNetEvent('bringplayer', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
     local xTarget = ESX.GetPlayerFromId(target)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         if xTarget then
             local targetCoords = xTarget.getCoords()
             local playerCoords = xPlayer.getCoords()
             savedCoords[target] = targetCoords
             xTarget.setCoords(playerCoords)
+            logs('Bring Player', GetPlayerName(source).. ' brought the player to him ' ..GetPlayerName(target))
         else
         end
     end
@@ -120,11 +127,12 @@ end)
 RegisterNetEvent('bringback', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
     local xTarget = ESX.GetPlayerFromId(target)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         if xTarget then
             local playerCoords = savedCoords[target]
             if playerCoords then
                 xTarget.setCoords(playerCoords)
+                logs('Bring Back', GetPlayerName(source).. ' returned the player to his last location ' ..GetPlayerName(target))
                 savedCoords[target] = nil
             else
             end
@@ -135,8 +143,9 @@ end)
 
 RegisterNetEvent('revive', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         TriggerClientEvent('esx_ambulancejob:revive', target)
+        logs('Revive', GetPlayerName(source).. ' has revived the player ' ..GetPlayerName(target))
     end
 end)
 
@@ -155,17 +164,34 @@ end)
 
 RegisterNetEvent('heal', function(target)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         TriggerClientEvent('esx_basicneeds:healPlayer', target)
+        logs('Heal', GetPlayerName(source).. ' has healed the player ' ..GetPlayerName(target))
     end
 end)
 
-RegisterServerEvent('setjob', function(job, grade, target)
+RegisterNetEvent('setjob', function(job, grade, target)
     local xPlayer = ESX.GetPlayerFromId(source)
-    if Config.DozvoljeneAdminGrupe[xPlayer.getGroup()] then
+    if Config.AllowedGroups[xPlayer.getGroup()] then
         local xTarget = ESX.GetPlayerFromId(target)
         xTarget.setJob(job, grade)
-        TriggerClientEvent('notification', target,
-            xTarget.getJob().label .. ' - ' .. xTarget.getJob().grade_label .. ' ' .. Strings.hasbeen)
+        TriggerClientEvent('notification', target, xTarget.getJob().label .. ' - ' .. xTarget.getJob().grade_label .. ' ' .. Strings.hasbeen)
+        logs('Set Job', GetPlayerName(source).. ' gave the job ' ..xTarget.getJob().label .. ' - ' .. xTarget.getJob().grade_label .. ' to the player ' ..GetPlayerName(target))
     end
 end)
+
+
+function logs(name, message, image)
+    local vrijeme = os.date('*t')
+        local poruka = {
+            {
+                ["color"] = 0,--
+                ["title"] = "".. name .."",
+                ["description"] = message,
+                ['image'] = {
+                    ['url'] = image or ""
+                }
+            }
+          }
+    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "Logovi", embeds = poruka, avatar_url = ""}), { ['Content-Type'] = 'application/json' })
+  end
